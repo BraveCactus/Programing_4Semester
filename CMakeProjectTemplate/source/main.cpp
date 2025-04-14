387af9b5b7bd11385ad1eb9223c4abda80e02e6c
MatveyBaranchugov <143385205+BraveCactus@users.noreply.github.com>
2025-04-14 19:18:40 +0300

    Delete CMakeProjectTemplate directory

--- CMakeProjectTemplate/source/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "C:\Users\matve\Desktop\Programing_4Semester\CMakeProjectTemplate\include\my_lib\sum.h"
-using namespace std;
-
-int main() {    
-    cout << sum(2, 4) << endl;
-    return 0;
-}
-
--- CMakeProjectTemplate/source/my_libs/sum.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "C:\Users\matve\Desktop\Programing_4Semester\CMakeProjectTemplate\include\my_lib\sum.h"
-
-double sum(double x, double y){
-    return x + y;
-}
