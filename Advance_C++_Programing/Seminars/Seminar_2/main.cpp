95c7eacb2fcb119ab45123ffa525f60b7f0e19a9
MatveyBaranchugov <mattdav961@gmail.com>
2025-05-09 23:07:28 +0300

    Update Seminar_2

--- Advance_C++_Programing/Seminars/Seminar_2/main.cpp
+++ Advance_C++_Programing/Seminars/Seminar_2/main.cpp
@@ -2,67 +2,74 @@
 #include <vector>
 using namespace std;
 
-//Агрегатная инициализация
-
-struct Simple{
+// Агрегатная инициализация - инициализация структуры/класса с публичными полями
+struct Simple {
     int a;
     float b;
-};
-
-Simple s = {.a = 4, .b = 5.7};
-
-class MyClassInterface{
 public:
-
-    virtual void func() = 0;
-    MyClassInterface() {}
-
-    virtual ~MyClassInterface() { cout << "MyClassInterface" << endl;}
- 
+    Simple(int val1, int val2): a{val1}, b{val2}{}
 };
 
+Simple s(4, 5.7); // Инициализация с указанием имен полей
+
+// Абстрактный базовый класс с чисто виртуальной функцией
+class MyClassInterface {
+public:
+    virtual void func() = 0; // Чисто виртуальная функция
+    MyClassInterface(){}
+    virtual ~MyClassInterface() { cout << "MyClassInterface destructor" << endl; }
+};
+
+// Производный класс, реализующий абстрактный метод
 class Derived : public MyClassInterface {
 public:
-    void func () override {}
-
-    Derived() {}
-    ~Derived() {cout << "Derived" << endl;}
-
-};
-
-template <typename T>
-class CRTPBase{
+    void func() override {} // Реализация виртуальной функции
     
-    void f () {
-        static_cast<T>(this)->g(); 
-    }
-
+    Derived() {}
+    ~Derived() { cout << "Derived destructor" << endl; }
 };
 
-class CRTPDerived : public CRTPBase <CRTPDerived>{
+// Шаблонный класс для паттерна CRTP (Curiously Recurring Template Pattern)
+template <typename T>
+class CRTPBase {
+public:
+    void f() {
+        static_cast<T*>(this)->g(); // Преобразование this к производному типу
+    }
+};
+
+// Производный класс от CRTPBase
+class CRTPDerived : public CRTPBase<CRTPDerived> {
     int a;
 public:
-    void g() {a = 0;}
+    void g() { a = 0; } // Реализация метода, ожидаемого базовым классом
 };
 
-class CRTPDerivedOther : public CRTPBase <CRTPDerived>{
+// Другой производный класс (исправленная версия)
+class CRTPDerivedOther : public CRTPBase<CRTPDerivedOther> { // Исправлен шаблонный параметр
     float a;
 public:
-    //void g() { a = 5.0;}
+    void g() { a = 5.0f; } // Реализация метода g
 };
 
-int main(){
-
-    Derived *d = new Derived();
-
-    delete d;
-
+int main() {
+    // Работа с полиморфизмом
+    Derived* d = new Derived();
+    delete d; // Вызовется деструктор Derived, затем MyClassInterface
+    
+    cout << "-----" << endl;
+    
     MyClassInterface* ptr = new Derived;
-
-    CRTPDerivedOther a;
-
-    //a.f(); !!!Не скомпилируется
-
-    delete ptr;
-
+    delete ptr; // Благодаря виртуальному деструктору вызовется правильный деструктор
+    
+    cout << "-----" << endl;
+    
+    // Работа с CRTP
+    CRTPDerived crtp1;
+    crtp1.f(); // Вызовет g() из CRTPDerived
+    
+    CRTPDerivedOther crtp2;
+    crtp2.f(); // Вызовет g() из CRTPDerivedOther
+    
+    return 0;
 }
